84967140c55583e886e2b2894c39a970186c5e60
bagreer <prausmi1@uhk.cz>
2026-05-05 20:39:20 +0200

    enabling JPA repos

--- src/main/java/com/example/pro2project/Pro2ProjectApplication.java
+++ src/main/java/com/example/pro2project/Pro2ProjectApplication.java
@@ -2,7 +2,11 @@ package com.example.pro2project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.example.pro2project.repositories")
+@EntityScan(basePackages = "com.example.pro2project.models")
 @SpringBootApplication
 public class Pro2ProjectApplication {
 
